17d0c86ea10b3dda60258290e4dad92c886301dc
Synkov2102 <synkoveugeny@gmail.com>
2022-05-12 19:28:59 +0300

    Разрешен доступ

--- backend/app.js
+++ backend/app.js
@@ -54,6 +54,7 @@ app.use((req, res, next) => {
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
+  res.header('Access-Control-Allow-Origin', origin);
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
@@ -66,6 +67,7 @@ app.use((req, res, next) => {
     if (allowedCors.includes(origin)) {
       res.header('Access-Control-Allow-Origin', origin);
     }
+    res.header('Access-Control-Allow-Origin', origin);
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     // разрешаем кросс-доменные запросы с этими заголовками
